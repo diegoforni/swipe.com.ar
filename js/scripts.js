3888eef407eb6b819c4fbc72f610c8f14c52ab9d
diegoforni <d.forni0204@gmail.com>
2022-11-14 13:51:46 -0300

    Debug?

--- js/scripts.js
+++ js/scripts.js
@@ -722,7 +722,7 @@ $(document).ready(function () {
 
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     // true for mobile device
-    var dontknowwhattodo = true;
+    document.getElementsById("contacto")[0].setAttribute("class", "iOS-device");;
   }else{
     function mousecursor() {
         if ($("body")) {
